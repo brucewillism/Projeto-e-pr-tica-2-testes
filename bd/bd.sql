e304728a93f09753be9b3a7e4cc06e1cd8be561a
thiagovodmr <tvnicius782@gmail.com>
2018-04-20 19:40:45 -0300

    dump do banco depois das alterações

--- bd/bd.sql
+++ bd/bd.sql
@@ -16,61 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `administrators`
---
-
-DROP TABLE IF EXISTS `administrators`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `administrators` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `administrators`
---
-
-LOCK TABLES `administrators` WRITE;
-/*!40000 ALTER TABLE `administrators` DISABLE KEYS */;
-INSERT INTO `administrators` VALUES (1,'Thiago Vinícius Oliveira De Matos Rodrigues','thiago','1234','tvnicius782@gmail.com');
-/*!40000 ALTER TABLE `administrators` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `administrators_health_units`
---
-
-DROP TABLE IF EXISTS `administrators_health_units`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `administrators_health_units` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `health_unit_id` int(10) unsigned NOT NULL,
-  `administrator_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `health_unit_id` (`health_unit_id`),
-  KEY `administrator_id` (`administrator_id`),
-  CONSTRAINT `administrators_health_units_ibfk_1` FOREIGN KEY (`health_unit_id`) REFERENCES `health_units` (`id`),
-  CONSTRAINT `administrators_health_units_ibfk_2` FOREIGN KEY (`administrator_id`) REFERENCES `administrators` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `administrators_health_units`
---
-
-LOCK TABLES `administrators_health_units` WRITE;
-/*!40000 ALTER TABLE `administrators_health_units` DISABLE KEYS */;
-/*!40000 ALTER TABLE `administrators_health_units` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `health_units`
 --
 
@@ -97,6 +42,60 @@ LOCK TABLES `health_units` WRITE;
 /*!40000 ALTER TABLE `health_units` DISABLE KEYS */;
 /*!40000 ALTER TABLE `health_units` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_health_units`
+--
+
+DROP TABLE IF EXISTS `users_health_units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_health_units` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `health_unit_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_rows` (`health_unit_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `users_health_units_ibfk_1` FOREIGN KEY (`health_unit_id`) REFERENCES `health_units` (`id`),
+  CONSTRAINT `users_health_units_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_health_units`
+--
+
+LOCK TABLES `users_health_units` WRITE;
+/*!40000 ALTER TABLE `users_health_units` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_health_units` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-17 18:45:42
+-- Dump completed on 2018-04-20 19:36:22
