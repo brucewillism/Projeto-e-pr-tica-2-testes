d9e38e13c37b891d91eec9206bee544f9cf352d4
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2018-07-20 18:37:48 -0300

    Update pg2.php

--- acessibilidade/pg2.php
+++ acessibilidade/pg2.php
@@ -1,25 +1,29 @@
 <?php include_once('header.php')?>
+<br>
 <center>
-<h3>Proposta de cards</h3>
-<h3>Comentarios implementados implementados direto pelo cake</h3>
-
+<h3>Policlínica São Lucas</h3>
 <div class="card a" style=" float:left; margin-left: 4em;margin-top: 2em;">
   <div class="card-header" style="background-color:#c8e1ff;">
-    Informações
+   <h5> Informações</h5>
   </div>
-    <p>Cras justo odio</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
   </ul>
 </div>
 <div class="card a" style="float:right;margin-left: 30em; margin-top: -15em; margin-right: 4em;">
-  <div class="card-header" style="background-color:#c8e1ff;">
-    Especialidades
+  <div class="card-header" style="background-color:#c8e1ff;"><h5>
+    Especialidades </h5>
   </div>
-    <p>Cras justo odio</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
   </ul>
 </div>
-<!-- <div style="width:50em;height:15em;border-color: grey; border-radius:1em;background-color: white; margin-top: 20em;"> -->
-<!-- </div>
- --><!-- <center>
+<div style="width:50em;height:15em;border-color: grey; border-radius:1em;background-color: white; margin-top: 20em;">
+	<h4>Comentários</h4>
+OBS: Mapa é opcional terá um botão no card se desejar vizualizar.
+</div>
+<br>
+<center>
 <div style="background-color: #c8e1ff; width: 50em; border-radius: 1em">
 <fieldset> <legend><h4>Deixe seu Comentário!</h4></legend>
   <h5>Avalie:</h5>
@@ -27,40 +31,30 @@
       <div class="estrelas">
         <input type="radio" id="vazio" name="estrela" value="" checked>
         
-        <label for="estrela_um"><i class="fas fa-star"></label>
+        <label for="estrela_um"><i class="fa fa-star"></label></i>
         <input type="radio" id="estrela_um" name="estrela" value="1"></i>
-        
-        <label for="estrela_dois"><i class="fas fa-star"></i></label>
+    
+        <label for="estrela_dois"><i class="fa fa-star"></i></label>
         <input type="radio" id="estrela_dois" name="estrela" value="2">
         
-        <label for="estrela_tres"><i class="fas fa-star"></i></label>
+        <label for="estrela_tres"><i class="fa fa-star"></i></label>
         <input type="radio" id="estrela_tres" name="estrela" value="3">
         
-        <label for="estrela_quatro"><i class="fas fa-star"></i></label>
+        <label for="estrela_quatro"><i class="fa fa-star"></i></label>
         <input type="radio" id="estrela_quatro" name="estrela" value="4">
         
-        <label for="estrela_cinco"><i class="fas fa-star"></i></label>
-        <input type="radio" id="estrela_cinco" name="estrela" value="5"><br><br>
-        <input type="submit" value="Enviar">
+        <label for="estrela_cinco"><i class="fa fa-star"></i></label>
+        <input type="radio" id="estrela_cinco" name="estrela" value="5">
+        <input type="submit" class="oi cc" style="border-style: none; color:white;background-color: #17a2b8!important;" value="Enviar">
       </div>
     </form>
     <br>
-<label for="Insira seu nome">
+  &nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;<label for="Insira seu nome">
 Nome:
-<input id="autor" name="autor" style="width: 25em"></label>
-</fieldset>
+<input id="autor" name="autor" style="width: 25em;"></label></fieldset>
 <label for="Insira seu comentário">
 Mensagem:
-<textarea id="mensagem" type="text-area" style="width: 25em" name="mensagem"></textarea></label>
-<button type="submit" style="">Enviar</button></fieldset>
+<textarea id="mensagem" type="text-area" style="width: 25em" name="mensagem"></textarea></label><br>
+<button  class="oi cc" type="submit" style="border-style: none; color:white;background-color: #17a2b8!important;">Enviar</button></fieldset>
 </center>
 <br>
-<div style="background-color: white;">
-   <h5>Conecte-se</h5>
-                <ul class="company-social">
-                  <p><a href="#"><i class="fa fa-facebook"></i></a></p>
-                  
-                  <p><a href="#"><i class="fa fa-twitter"></i></a></li></p>
-                  <p><a href="#"><i class="fa fa-google-plus"></i></a></p>
-                  
-</div> -->
