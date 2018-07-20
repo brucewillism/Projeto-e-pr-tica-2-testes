7b9ce105b7b42f2e85b563626398ed69373fd636
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2018-07-20 18:27:51 -0300

    Create footer.php

---
+++ acessibilidade/footer.php
@@ -0,0 +1,7 @@
+
+<div style="background-color: white;">
+   <h5>Conecte-se!</h5>
+  <p><a href="#"><i class="fa fa-facebook" style="font-size:28px;"></i></a>&nbsp;&nbsp;
+     <a href="#"><i class="fa fa-twitter" style="font-size:28px;"></i></a></li>&nbsp;&nbsp;
+     <a href="#"><i class="fa fa-google-plus" style="font-size:28px;"></i></a></p>          
+</div>
