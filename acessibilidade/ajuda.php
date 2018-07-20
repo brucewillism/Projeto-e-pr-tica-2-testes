a6c87df9f0c48ab3a2708aceba2096a9e5a84ff7
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2018-07-20 19:43:59 -0300

    Create ajuda.php

---
+++ acessibilidade/ajuda.php
@@ -0,0 +1,25 @@
+<?php include_once('header.php')?>
+<center>
+	<h2>Seja bem-vindo(a)!</h2>
+	<h3>Em nosso sistema temos alguns atalhos para facilitar a utilização</h3>
+</center>
+<h4>Para realizar a pesquisa das Unidades Básicas de Saúde de Igarassu na página inicial:<p>
+ No navegador IE, Chrome, Safari, Opera 15+: [ALT] + p<br>
+ Firefox: [ALT] [SHIFT] + p
+ Opera prior version 15: [SHIFT] [ESC] + p<br>
+
+Na página da Unidade para ir direto as informações:<p><br>
+ No navegador IE, Chrome, Safari, Opera 15+: [ALT] + i<br>
+ Firefox: [ALT] [SHIFT] + i
+ Opera prior version 15: [SHIFT] [ESC] + i<br>
+
+Na página da Unidade para fazer comentários:<p><br>
+ No navegador IE, Chrome, Safari, Opera 15+: [ALT] + c<br>
+ Firefox: [ALT] [SHIFT] + i
+ Opera prior version 15: [SHIFT] [ESC] + i<br>
+
+ <h3>Como navegar?</h3>
+ <h4> Na tela inicial clicando na barra de pesquisa você pode inserir nome, cidade, CEP da Unidade que você procura. Depois basta clicar em pesquisar, em seguida, clicar na Unidade abaixo se ela existir. Você irá para página da Unidade a qual conterá as informações da Unidade e Especialidades, mais abaixo os comentários sobre tais informações, e por fim um espaço onde você pode avaliar e adicionar mais comentários. Boa sorte! </h4>
+
+
+<?php include_once('footer.php')?>
